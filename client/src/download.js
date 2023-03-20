2b01604bba8cafc9935914337a1de1a8ded82344
Kiki <kiki@pop-os.localdomain>
2023-03-20 13:12:34 +0700

    update

--- client/src/download.js
+++ client/src/download.js
@@ -3,26 +3,38 @@ import jsPDF from "jspdf";
 import "jspdf-autotable";
 
 export default function downloadPdf() {
-  // Get friend data from API
-  API
-    .get("/friends")
+  API.get("/friends")
     .then((response) => {
       const friends = response.data.data;
-      // Create PDF document using jsPDF and add table
       const doc = new jsPDF();
+      const canvas = document.getElementById("chart");
+      const chartData = canvas.toDataURL();
+
       doc.autoTable({
         head: [["Name", "Gender", "Age"]],
         body: friends?.map((friend) => [friend.name, friend.gender, friend.age]),
       });
 
-      // Add chart as image to PDF document
-      const canvas = document.getElementById("chart");
-      const chartData = canvas.toDataURL();
       doc.addPage();
       doc.addImage(chartData, "PNG", 10, 10, 180, 100);
 
-      // Download PDF file
-      doc.save("friends.pdf");
+      API.get("/friendstats")
+        .then((response) => {
+          doc.autoTable({
+            margin: { top: 120 },
+            head: [["Total Friend", "Male Count", "Female Count", "Under 19 Count", "Above 20 Count"]],
+            body: [[
+              response.data.data.total_friend_count,
+              response.data.data.male_count,
+              response.data.data.female_count,
+              response.data.data.under_19_count,
+              response.data.data.above_20_count,
+            ]],
+          });
+
+          doc.save("friends.pdf");
+        })
+        .catch((error) => console.log(error));
     })
     .catch((error) => console.log(error));
 }
